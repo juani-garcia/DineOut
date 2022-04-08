c8fd2f45a5c1828c6567fdb34ba6f3a04682d7f2
shoni <juanigarcia@itba.edu.ar>
2022-04-08 14:29:50 -0300

    Add restaurant service interface

---
+++ interfaces/src/main/java/ar/edu/itba/paw/service/RestaurantService.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.paw.service;
+
+import ar.edu.itba.paw.model.Restaurant;
+
+import java.util.Optional;
+
+public interface RestaurantService {
+
+    Optional<Restaurant> getRestaurantById(long id);
+
+}
