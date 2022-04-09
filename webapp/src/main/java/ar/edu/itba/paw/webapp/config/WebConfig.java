648ee63506ed0d94ec4e4f9f437464987f8096c6
shoni <juanigarcia@itba.edu.ar>
2022-04-09 10:48:07 -0300

    Add sql schema to project

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,10 +1,14 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -12,12 +16,17 @@ import org.springframework.web.servlet.view.JstlView;
 
 import javax.sql.DataSource;
 
-@ComponentScan({"ar.edu.itba.paw.webapp.controller",
+@ComponentScan({
+        "ar.edu.itba.paw.webapp.controller",
         "ar.edu.itba.paw.service",
-        "ar.edu.itba.paw.persistence"})
+        "ar.edu.itba.paw.persistence"
+})
 @EnableWebMvc
 @Configuration
 public class WebConfig {
+
+    @Value ("classpath:sql/schema.sql")
+    private Resource schemaSql;
 
     @Bean
     public ViewResolver viewResolver() {
@@ -39,6 +48,21 @@ public class WebConfig {
         ds.setPassword("paw-2022a-10");
 
         return ds;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer() {
+        DataSourceInitializer dsi = new DataSourceInitializer();
+        dsi.setDataSource(dataSource());
+        dsi.setDatabasePopulator(databasePopulator());
+        return dsi;
+    }
+
+    @Bean
+    public DatabasePopulator databasePopulator() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(schemaSql);
+        return populator;
     }
 
 }
