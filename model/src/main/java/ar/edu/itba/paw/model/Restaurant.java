d031ca7204f9c2b2c2b52b598dbe0331b8d81f1b
shoni <juanigarcia@itba.edu.ar>
2022-04-11 09:43:10 -0300

    Add mail field to restaurant model

--- model/src/main/java/ar/edu/itba/paw/model/Restaurant.java
+++ model/src/main/java/ar/edu/itba/paw/model/Restaurant.java
@@ -2,14 +2,15 @@ package ar.edu.itba.paw.model;
 
 public class Restaurant {
     private final long id;
-    private final String name, address, detail, image;
+    private final String name, mail, address, detail, image;
 
-    public Restaurant(long id, String name, String address, String detail, String image) {
+    public Restaurant(long id, String name, String address, String mail, String detail, String image) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.detail = detail;
         this.image = image;
+        this.mail = mail;
     }
 
     public long getId() {
@@ -30,5 +31,9 @@ public class Restaurant {
 
     public String getImage() {
         return image;
+    }
+
+    public String getMail() {
+        return mail;
     }
 }
