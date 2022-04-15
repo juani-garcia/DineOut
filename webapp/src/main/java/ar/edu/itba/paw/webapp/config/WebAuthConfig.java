65c33a9f0fb3ea95cef2773b468c2511f8f5fe5a
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-04-15 19:19:27 -0300

    Customize configure methods for WebAuthConfig

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -2,8 +2,11 @@ package ar.edu.itba.paw.webapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.util.concurrent.TimeUnit;
 
 @EnableWebSecurity
 @Configuration
@@ -11,7 +14,30 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.sessionManagement().invalidSessionUrl("login").and().authorizeRequests().antMatchers("/login");
+        http.sessionManagement()
+                .invalidSessionUrl("/login")
+            .and().authorizeRequests()
+                .antMatchers("/login", "/register").anonymous()
+                .antMatchers("/post/edit").hasRole("EDITOR")
+                .antMatchers("/**").authenticated()
+            .and().formLogin()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/", false)
+                .loginPage("/login")
+            .and().rememberMe()
+                .rememberMeParameter("rememberme")
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
+            .and().logout()
+                .logoutUrl("logout")
+                .logoutSuccessUrl("login")
+            .and().exceptionHandling()
+                .accessDeniedPage("/403")
+            .and().csrf().disable();
     }
 
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/media/**");
+    }
 }
