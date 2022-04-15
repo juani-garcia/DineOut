de78f37cf2f8cbf62ee2fd1aeacefc8ac39a1c7e
BraveJero <jbrave271@gmail.com>
2022-04-15 18:24:19 -0300

    add security dependencies

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -1,8 +1,12 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
+@Configuration
 public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
