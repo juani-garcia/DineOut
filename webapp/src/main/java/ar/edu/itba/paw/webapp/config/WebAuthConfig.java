9382b39810763bc9f5a525bd2a26ae6c976eeb1f
shoni <juanigarcia@itba.edu.ar>
2022-04-15 12:14:03 -0300

    Fix typo

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -1,12 +1,13 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.sessionManagment().invalidSessionUrl("login").and().authorizeRequests().antMatchers("/login")
+        http.sessionManagement().invalidSessionUrl("login").and().authorizeRequests().antMatchers("/login");
     }
 
 }
