bd519800a583dd84ff2c10a3dd0ee00bfec15399
shoni <juanigarcia@itba.edu.ar>
2022-04-15 18:20:46 -0300

    Separate webapp controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ReservationController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ReservationController.java
@@ -1,25 +1,19 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import ar.edu.itba.paw.model.Restaurant;
 import ar.edu.itba.paw.service.ReservationService;
 import ar.edu.itba.paw.service.RestaurantService;
 import ar.edu.itba.paw.webapp.exceptions.RestaurantNotFoundException;
 import ar.edu.itba.paw.webapp.form.ReservationForm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 @Controller
-public class WebappController {
+public class ReservationController {
 
     @Autowired
     private RestaurantService restaurantService;
@@ -27,22 +21,9 @@ public class WebappController {
     @Autowired
     private ReservationService reservationService;
 
-    @ExceptionHandler(RestaurantNotFoundException.class)
-    @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public ModelAndView noSuchRestaurant() {
-        return new ModelAndView("restaurant_not_found");
-    }
-
-    @RequestMapping(value = "/")
-    public ModelAndView webapp(@RequestParam(name = "page", defaultValue = "1") final int page) {
-        final ModelAndView mav = new ModelAndView("index");
-        mav.addObject("restaurants", restaurantService.getAll(page));
-        return mav;
-    }
-
     @RequestMapping("/reserve/{resId}")
     public ModelAndView reservation(@PathVariable final long resId, @ModelAttribute("reservationForm") final ReservationForm form) {
-         final ModelAndView mav = new ModelAndView("reservation");
+         final ModelAndView mav = new ModelAndView("reservation/reservation");
 
          mav.addObject("restaurant", restaurantService.getById(resId).orElseThrow(RestaurantNotFoundException::new));
          return mav;
