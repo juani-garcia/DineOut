2492173818b1e2c39d451c65256c86217a65e3e2
shoni <juanigarcia@itba.edu.ar>
2022-04-16 02:34:06 -0300

    Add category service

---
+++ service/src/main/java/ar/edu/itba/paw/service/CategoryServiceImpl.java
@@ -0,0 +1,31 @@
+package ar.edu.itba.paw.service;
+
+import ar.edu.itba.paw.model.Category;
+import ar.edu.itba.paw.persistence.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @Override
+    public Optional<Category> getById(long id) {
+        return categoryDao.getById(id);
+    }
+
+    @Override
+    public List<Category> getByRestaurantId(long restaurantId) {
+        return categoryDao.getByRestaurantId(restaurantId);
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return categoryDao.getAll();
+    }
+}
