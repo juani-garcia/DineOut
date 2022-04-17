bb951e17b4d0ace20a7391351c47535d19ef79b5
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-04-17 02:12:35 -0300

    Develop register and login mavs

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
@@ -1,15 +1,24 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.model.User;
 import ar.edu.itba.paw.service.RestaurantService;
+import ar.edu.itba.paw.service.UserService;
 import ar.edu.itba.paw.webapp.exceptions.RestaurantNotFoundException;
+import ar.edu.itba.paw.webapp.form.UserForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private RestaurantService restaurantService;
@@ -25,6 +34,22 @@ public class HomeController {
         final ModelAndView mav = new ModelAndView("home/index");
         mav.addObject("restaurants", restaurantService.getAll(page));
         return mav;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView registerForm(@ModelAttribute("registerForm") final UserForm form) {
+        return new ModelAndView("home/register");
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView register(@Valid @ModelAttribute("registerForm") final UserForm form, final BindingResult errors) {
+        if (errors.hasErrors()) {
+            return registerForm(form);
+        }
+
+        final User user = userService.create(form.getUsername(), form.getPassword());
+
+        return new ModelAndView("redirect:/profile/" + user.getId());
     }
 
     @RequestMapping("/login")
