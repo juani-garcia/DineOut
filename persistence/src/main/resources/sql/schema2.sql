97f8e1168c2a2f63cfc5cdbd3297008d73bf8338
BraveJero <jbrave271@gmail.com>
2022-04-21 16:39:42 -0300

    Add schema to update tables, remove before merge to master

---
+++ persistence/src/main/resources/sql/schema2.sql
@@ -0,0 +1,4 @@
+-- Add user reference to restaurant.
+ALTER TABLE restaurant ADD COLUMN user_id INTEGER DEFAULT null;
+ALTER TABLE restaurant ADD FOREIGN KEY (user_id) REFERENCES account(id) ON DELETE CASCADE;
+
