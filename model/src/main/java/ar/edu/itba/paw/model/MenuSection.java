598433bac37f3472b68ec43c7219edf9c47c10e2
BraveJero <jbrave271@gmail.com>
2022-04-22 09:11:21 -0300

    Add item list to section model

--- model/src/main/java/ar/edu/itba/paw/model/MenuSection.java
+++ model/src/main/java/ar/edu/itba/paw/model/MenuSection.java
@@ -1,14 +1,34 @@
 package ar.edu.itba.paw.model;
 
+import java.util.List;
+
 public class MenuSection {
 
-    private long id, restaurantId;
+    private long id, restaurantId, ordering;
     private String name;
+    private List<MenuItem> menuItemList;
 
-    public MenuSection(long id, String name, long restaurantId) {
+    public MenuSection(long id, String name, long restaurantId, long ordering) {
         this.id = id;
         this.name = name;
         this.restaurantId = restaurantId;
+        this.ordering = ordering;
+    }
+
+    public long getOrdering() {
+        return ordering;
+    }
+
+    public void setOrdering(long ordering) {
+        this.ordering = ordering;
+    }
+
+    public List<MenuItem> getMenuItemList() {
+        return menuItemList;
+    }
+
+    public void setMenuItemList(List<MenuItem> menuItemList) {
+        this.menuItemList = menuItemList;
     }
 
     public long getId() {
