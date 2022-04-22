6b3d2540b13b414f7a86f6a4860eb65e91f5355c
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-04-22 20:21:16 -0300

    Create Image service

---
+++ interfaces/src/main/java/ar/edu/itba/paw/service/ImageService.java
@@ -0,0 +1,17 @@
+package ar.edu.itba.paw.service;
+
+import ar.edu.itba.paw.model.Image;
+
+import java.util.Optional;
+
+public interface ImageService {
+
+    Image create(final long id, final byte[] source);
+
+    Optional<Image> getById(final long id);
+
+    boolean edit(final long id, final byte[] source);
+
+    boolean delete(final long id);
+
+}
