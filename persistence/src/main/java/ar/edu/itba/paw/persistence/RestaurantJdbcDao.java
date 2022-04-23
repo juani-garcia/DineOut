7adb4c3039cd1d5925b1b6c1bd44d407f24fc4e0
shoni <juanigarcia@itba.edu.ar>
2022-04-23 20:17:16 -0300

    Add method filter to dao

--- persistence/src/main/java/ar/edu/itba/paw/persistence/RestaurantJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/RestaurantJdbcDao.java
@@ -1,6 +1,9 @@
 package ar.edu.itba.paw.persistence;
 
+import ar.edu.itba.paw.model.Category;
+import ar.edu.itba.paw.model.OpeningHours;
 import ar.edu.itba.paw.model.Restaurant;
+import ar.edu.itba.paw.model.Zone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -8,10 +11,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public class RestaurantJdbcDao implements RestaurantDao {
@@ -22,7 +22,7 @@ public class RestaurantJdbcDao implements RestaurantDao {
     /* private X default=package-private for testing */
     static final RowMapper<Restaurant> ROW_MAPPER = (rs, rowNum) ->
             new Restaurant(rs.getLong("id"), rs.getLong("user_id"), rs.getString("name"), rs.getString("address"),
-            rs.getString("mail"), rs.getString("detail"));
+            rs.getString("mail"), rs.getString("detail"), Zone.getByOrdinal(rs.getLong("zone_id")));
 
     @Autowired
     public RestaurantJdbcDao(final DataSource ds) {
@@ -48,6 +48,60 @@ public class RestaurantJdbcDao implements RestaurantDao {
     }
 
     @Override
+    public List<Restaurant> filter(int page, String name, List<Category> categories, List<OpeningHours> openingHours, List<Zone> zones) {
+        List<Object> args = new ArrayList<>();
+
+        StringBuilder sql = new StringBuilder("SELECT DISTINCT r.id AS id, r.name AS name, r.address AS address,");
+        sql.append("r.mail AS mail, r.detail AS detail, r.zone_id AS zone_id, r.user_id AS user_id\n");
+
+        sql.append("FROM (restaurant r LEFT OUTER JOIN restaurant_category rc ON r.id = rc.restaurant_id)\n");
+        sql.append("LEFT OUTER JOIN restaurant_opening_hours roh ON r.id = roh.restaurant_id\n");
+        sql.append("WHERE true\n");
+
+        if(name != null) {
+            sql.append("AND LOWER(r.name) like ?\n");
+            args.add('%' + name.toLowerCase() + '%');
+        }
+
+        // TODO:
+        if(categories != null) {
+            sql.append("AND (");
+            for(Category category : categories) {
+                sql.append(" rc.category_id = ? OR");
+                args.add(category.ordinal());
+            }
+            sql.delete(sql.length() - 2, sql.length());
+            sql.append(")\n");
+        }
+
+        if(openingHours != null) {
+            sql.append("AND (");
+            for(OpeningHours hours : openingHours) {
+                sql.append(" roh.opening_hours_id = ? OR");
+                args.add(hours.ordinal());
+            }
+            sql.delete(sql.length() - 2, sql.length());
+            sql.append(")\n");
+        }
+
+        if(zones != null) {
+            sql.append("AND (");
+            for(Zone zone : zones) {
+                sql.append(" r.zone_id = ? OR");
+                args.add(zone.ordinal());
+            }
+            sql.delete(sql.length() - 2, sql.length());
+            sql.append(")\n");
+        }
+
+        sql.append("LIMIT ? OFFSET ?");
+        args.add(PAGE_SIZE);
+        args.add((page - 1) * PAGE_SIZE);
+
+        return jdbcTemplate.query(sql.toString(), args.toArray(), ROW_MAPPER);
+    }
+
+    @Override
     public Restaurant create(final long userID, final String name, final String address, final String mail, final String detail) {
         final Map<String, Object> restaurantData = new HashMap<>();
         restaurantData.put("user_id", userID);
@@ -57,6 +111,6 @@ public class RestaurantJdbcDao implements RestaurantDao {
         restaurantData.put("detail", detail);
 
         final long restaurantid = jdbcInsert.executeAndReturnKey(restaurantData).longValue();
-        return new Restaurant(restaurantid, userID, name, address, mail, detail);
+        return new Restaurant(restaurantid, userID, name, address, mail, detail, null);
     }
 }
