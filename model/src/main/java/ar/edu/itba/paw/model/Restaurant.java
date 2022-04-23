a7abafb73f4c9406314c5d2c15993b2379190c1f
shoni <juanigarcia@itba.edu.ar>
2022-04-23 20:18:02 -0300

    Add zone field to restaurant model

--- model/src/main/java/ar/edu/itba/paw/model/Restaurant.java
+++ model/src/main/java/ar/edu/itba/paw/model/Restaurant.java
@@ -3,14 +3,16 @@ package ar.edu.itba.paw.model;
 public class Restaurant {
     private final long id, userID;
     private final String name, mail, address, detail;
+    private final Zone zone;
 
-    public Restaurant(long id, long userID, String name, String address, String mail, String detail) {
+    public Restaurant(long id, long userID, String name, String address, String mail, String detail, Zone zone) {
         this.id = id;
         this.userID = userID;
         this.name = name;
         this.address = address;
         this.detail = detail;
         this.mail = mail;
+        this.zone = zone;
     }
 
     public long getId() {
@@ -29,9 +31,9 @@ public class Restaurant {
         return detail;
     }
 
-//    public String getImage() {
-//        return image;
-//    }
+    public Zone getZone() {
+        return zone;
+    }
 
     public long getUserID() {
         return userID;
