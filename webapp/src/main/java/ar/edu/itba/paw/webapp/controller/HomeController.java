1b403e9b12cb64511bb90955bf39eea14e850ad7
BraveJero <jbrave271@gmail.com>
2022-04-25 15:25:44 -0300

    Add role when creating user

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
@@ -1,17 +1,26 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.model.UserRole;
+import ar.edu.itba.paw.model.UserToRole;
 import ar.edu.itba.paw.service.RestaurantService;
+import ar.edu.itba.paw.service.UserRoleService;
 import ar.edu.itba.paw.service.UserService;
+import ar.edu.itba.paw.service.UserToRoleService;
 import ar.edu.itba.paw.webapp.form.UserForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -21,6 +30,19 @@ public class HomeController {
 
     @Autowired
     private RestaurantService restaurantService;
+
+    @Autowired
+    private UserRoleService userRoleService;
+
+    @Autowired
+    private UserToRoleService userToRoleService;
+
+    private final HashMap<String, String> roles = new HashMap<String, String>() {
+        {
+            put("RESTAURANT", "Mi restaurante.");
+            put("DINER", "Uso personal.");
+        }
+    };
 
     @RequestMapping(value = "/")
     public ModelAndView webapp() {
@@ -37,7 +59,12 @@ public class HomeController {
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public ModelAndView registerForm(@ModelAttribute("registerForm") final UserForm form) {
-        return new ModelAndView("register/register");
+        final ModelAndView mav =new ModelAndView("register/register");
+        List<String> roles = new ArrayList<>();
+        roles.add(this.roles.get("RESTAURANT"));
+        roles.add(this.roles.get("DINER"));
+        mav.addObject("roleItems", roles);
+        return mav;
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
@@ -47,6 +74,16 @@ public class HomeController {
         }
 
         final User user = userService.create(form.getUsername(), form.getPassword(), form.getFirstName(), form.getLastName());
+
+        if (form.getRole().equals(this.roles.get("RESTAURANT"))) {
+            Optional<UserRole> userRole = userRoleService.getByRoleName("RESTAURANT");
+            if (!userRole.isPresent()) throw new IllegalStateException("El rol RESTAURANT no esta presente en la bbdd");
+            userToRoleService.create(user.getId(), userRole.get().getId());
+        } else {
+            Optional<UserRole> userRole = userRoleService.getByRoleName("DINER");
+            if (!userRole.isPresent()) throw new IllegalStateException("El rol DINER no esta presente en la bbdd");
+            userToRoleService.create(user.getId(), userRole.get().getId());
+        }
 
         return new ModelAndView("redirect:/profile/" + user.getId());
     }
