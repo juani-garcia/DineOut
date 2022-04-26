88dfac80e8a9c2d1eb1d688ebdf62985164375e1
shoni <juanigarcia@itba.edu.ar>
2022-04-26 00:26:42 -0300

    Add start and end to shifts

--- model/src/main/java/ar/edu/itba/paw/model/OpeningHours.java
+++ model/src/main/java/ar/edu/itba/paw/model/OpeningHours.java
@@ -1,21 +1,25 @@
 package ar.edu.itba.paw.model;
 
+import java.time.LocalTime;
+import java.util.List;
+
 public enum OpeningHours {
-    MORNING     (1, "opening_hours.name.morning"),
-    NOON        (2, "opening_hours.name.noon"),
-    AFTERNOON   (3, "opening_hours.name.afternoon"),
-    EVENING     (4, "opening_hours.name.evening");
+    MORNING("opening_hours.name.morning", LocalTime.of(8, 0), LocalTime.of(11, 59)),
+    NOON("opening_hours.name.noon", LocalTime.of(12, 0), LocalTime.of(15, 59)),
+    AFTERNOON("opening_hours.name.afternoon", LocalTime.of(16, 0), LocalTime.of(19, 59)),
+    EVENING("opening_hours.name.evening", LocalTime.of(20, 0), LocalTime.of(23, 59));
 
-    private final long id;
     private final String message;
+    private final LocalTime start, end;
 
-    OpeningHours(long id, String message) {
-        this.id = id;
+    OpeningHours(String message, LocalTime start, LocalTime end) {
         this.message = message;
+        this.start = start;
+        this.end = end;
     }
 
     public long getId() {
-        return id;
+        return ordinal();
     }
 
     public String getMessage() {
@@ -23,11 +27,25 @@ public enum OpeningHours {
     }
 
     public static OpeningHours getById(long id) {
-        for(OpeningHours hours : OpeningHours.values()) {
-            if(hours.getId() == id) {
+        for (OpeningHours hours : OpeningHours.values()) {
+            if (hours.getId() == id) {
                 return hours;
             }
         }
         return null;
+    }
+
+    private boolean belongs(LocalTime time) {
+        return (time.isAfter(start) || time.equals(start)) &&
+                (time.isBefore(end) || time.equals(end));
+    }
+
+    public static boolean belongs(List<OpeningHours> hours, LocalTime time) {
+        for (OpeningHours hour : hours) {
+            if(hour.belongs(time)){
+                return true;
+            }
+        }
+        return false;
     }
 }
