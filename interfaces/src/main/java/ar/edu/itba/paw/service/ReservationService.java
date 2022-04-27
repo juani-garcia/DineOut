b4260e8089350b527374c3fd65eca4ef41c19076
BraveJero <jbrave271@gmail.com>
2022-04-27 16:28:24 -0300

    Add reservations getters to dao

--- interfaces/src/main/java/ar/edu/itba/paw/service/ReservationService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/service/ReservationService.java
@@ -3,9 +3,12 @@ package ar.edu.itba.paw.service;
 import ar.edu.itba.paw.model.Reservation;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ReservationService {
 
     Reservation create(long restaurantId, String userMail, int amount, LocalDateTime dateTime, String comments);
+
+    List<Reservation> getAllByUsername(String username);
 
 }
--- persistence/src/main/java/ar/edu/itba/paw/persistence/ReservationJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/ReservationJdbcDao.java
@@ -1,14 +1,18 @@
 package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.model.Reservation;
+import ar.edu.itba.paw.model.Restaurant;
+import ar.edu.itba.paw.model.Zone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -16,6 +20,9 @@ public class ReservationJdbcDao implements ReservationDao {
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert jdbcInsert;
+    static final RowMapper<Reservation> ROW_MAPPER = (rs, rowNum) ->
+            new Reservation(rs.getLong("reservation_id"), rs.getLong("restaurant_id"), rs.getString("user_mail"), rs.getInt("amount"),
+                    rs.getObject("date_time", LocalDateTime.class), rs.getString("comments"));
 
     @Autowired
     public ReservationJdbcDao(final DataSource ds) {
@@ -34,5 +41,10 @@ public class ReservationJdbcDao implements ReservationDao {
         final int reservationId = jdbcInsert.executeAndReturnKey(reservationData).intValue();
 
         return new Reservation(reservationId, restaurantId, userMail, amount, dateTime, comments);
+    }
+
+    @Override
+    public List<Reservation> getAllByUsername(String username) {
+        return jdbcTemplate.query("SELECT * FROM reservation WHERE user_mail = ? ORDER BY date_time", new Object[] {username},  ROW_MAPPER);
     }
 }
