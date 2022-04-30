58533c441d0c4d68fc9852c026be9a0eb1a29116
shoni <juanigarcia@itba.edu.ar>
2022-04-30 18:41:32 -0300

    Add method to catch duplicated usernames

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -1,12 +1,13 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.model.exceptions.DuplicatedUsernameException;
 import ar.edu.itba.paw.model.exceptions.RestaurantNotFoundException;
+import ar.edu.itba.paw.webapp.form.UserForm;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@ControllerAdvice
 public class ErrorController {
 
     @ExceptionHandler(RestaurantNotFoundException.class)
@@ -18,5 +19,19 @@ public class ErrorController {
     @RequestMapping("/403")
     public ModelAndView accessDenied() {
         return new ModelAndView("error/errorPage403");
+    }
+
+    @ExceptionHandler(DuplicatedUsernameException.class)
+    public ModelAndView duplicatedUsername(DuplicatedUsernameException ex) {
+        ModelAndView mav = new ModelAndView("register/register");
+        UserForm form = new UserForm();
+        form.setUsername(ex.getUsername());
+        form.setPassword(ex.getPassword());
+        form.setConfirmPassword(ex.getPassword());
+        form.setFirstName(ex.getFirstName());
+        form.setLastName(ex.getLastName());
+        mav.addObject("registerForm", form);
+        mav.addObject("duplicatedUsername", true);
+        return mav;
     }
 }
