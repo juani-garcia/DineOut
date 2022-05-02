91d29a498f6de7d96d7851b6ad8836467cfbb0f2
shoni <juanigarcia@itba.edu.ar>
2022-05-02 19:14:17 -0300

    Change filter to look for ids

--- service/src/main/java/ar/edu/itba/paw/service/RestaurantServiceImpl.java
+++ service/src/main/java/ar/edu/itba/paw/service/RestaurantServiceImpl.java
@@ -35,10 +35,10 @@ public class RestaurantServiceImpl implements RestaurantService {
     }
 
     @Override
-    public List<Restaurant> filter(int page, String name, String categoryName, String shiftName, String zoneName) {
-        Category category = Category.getByName(categoryName);
-        Zone zone = Zone.getByName(zoneName);
-        Shift shift = Shift.getByName(shiftName);
+    public List<Restaurant> filter(int page, String name, int categoryId, int shiftId, int zoneId) {
+        Category category = Category.getById(categoryId);
+        Zone zone = Zone.getById(zoneId);
+        Shift shift = Shift.getById(shiftId);
 
         return restaurantDao.filter(page, name, category, shift, zone);
     }
@@ -58,7 +58,7 @@ public class RestaurantServiceImpl implements RestaurantService {
 
         Restaurant restaurant = restaurantDao.create(userID, name, address, mail, detail, zone);
         for (Long categoryId : categories) {
-            Category category = Category.getByOrdinal(categoryId);
+            Category category = Category.getById(categoryId);
             categoryService.add(restaurant.getId(), category);
         }
         return restaurant;
