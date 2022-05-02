4f53b864a7810ccc83d6f82c5e5ccab545549597
shoni <juanigarcia@itba.edu.ar>
2022-05-01 21:49:20 -0300

    Add method to catch duplicated emails

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -1,7 +1,11 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.model.Category;
+import ar.edu.itba.paw.model.Zone;
+import ar.edu.itba.paw.model.exceptions.DuplicatedMailException;
 import ar.edu.itba.paw.model.exceptions.DuplicatedUsernameException;
 import ar.edu.itba.paw.model.exceptions.RestaurantNotFoundException;
+import ar.edu.itba.paw.webapp.form.RestaurantForm;
 import ar.edu.itba.paw.webapp.form.UserForm;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +36,23 @@ public class ErrorController {
         form.setLastName(ex.getLastName());
         mav.addObject("registerForm", form);
         mav.addObject("duplicatedUsername", true);
+        return mav;
+    }
+
+    @ExceptionHandler(DuplicatedMailException.class)
+    public ModelAndView duplicatedMail(DuplicatedMailException ex) {
+        ModelAndView mav = new ModelAndView("register/register_restaurant");
+        RestaurantForm form = new RestaurantForm();
+        form.setAddress(ex.getAddress());
+        form.setCategories(ex.getCategories());
+        form.setEmail(ex.getMail());
+        form.setZone(ex.getZone().getName());
+        form.setName(ex.getName());
+        form.setDetail(ex.getDetail());
+        mav.addObject("restaurantForm", form);
+        mav.addObject("duplicatedMail", true);
+        mav.addObject("zones", Zone.values());
+        mav.addObject("categoryList", Category.values());
         return mav;
     }
 }
