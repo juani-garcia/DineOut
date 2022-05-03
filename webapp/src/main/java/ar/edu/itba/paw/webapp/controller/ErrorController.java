25228d00386c8fad61229cbb66a6d73e24c6efe3
shoni <juanigarcia@itba.edu.ar>
2022-05-02 23:53:07 -0300

    Remove unnecessary method

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -1,10 +1,6 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import ar.edu.itba.paw.model.Category;
-import ar.edu.itba.paw.model.Zone;
-import ar.edu.itba.paw.model.exceptions.DuplicatedMailException;
 import ar.edu.itba.paw.model.exceptions.RestaurantNotFoundException;
-import ar.edu.itba.paw.webapp.form.RestaurantForm;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,20 +19,4 @@ public class ErrorController {
         return new ModelAndView("error/errorPage403");
     }
 
-    @ExceptionHandler(DuplicatedMailException.class)
-    public ModelAndView duplicatedMail(DuplicatedMailException ex) {
-        ModelAndView mav = new ModelAndView("register/register_restaurant");
-        RestaurantForm form = new RestaurantForm();
-        form.setAddress(ex.getAddress());
-        form.setCategories(ex.getCategories());
-        form.setEmail(ex.getMail());
-        form.setZone(ex.getZone().getName());
-        form.setName(ex.getName());
-        form.setDetail(ex.getDetail());
-        mav.addObject("restaurantForm", form);
-        mav.addObject("duplicatedMail", true);
-        mav.addObject("zones", Zone.values());
-        mav.addObject("categoryList", Category.values());
-        return mav;
-    }
 }
