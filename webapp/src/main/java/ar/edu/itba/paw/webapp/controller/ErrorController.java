15fad4d21831b18ba3fab816898996c094d34d79
shoni <juanigarcia@itba.edu.ar>
2022-05-02 23:15:45 -0300

    Remove unnecessary method

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -3,10 +3,8 @@ package ar.edu.itba.paw.webapp.controller;
 import ar.edu.itba.paw.model.Category;
 import ar.edu.itba.paw.model.Zone;
 import ar.edu.itba.paw.model.exceptions.DuplicatedMailException;
-import ar.edu.itba.paw.model.exceptions.DuplicatedUsernameException;
 import ar.edu.itba.paw.model.exceptions.RestaurantNotFoundException;
 import ar.edu.itba.paw.webapp.form.RestaurantForm;
-import ar.edu.itba.paw.webapp.form.UserForm;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,20 +21,6 @@ public class ErrorController {
     @RequestMapping("/403")
     public ModelAndView accessDenied() {
         return new ModelAndView("error/errorPage403");
-    }
-
-    @ExceptionHandler(DuplicatedUsernameException.class)
-    public ModelAndView duplicatedUsername(DuplicatedUsernameException ex) {
-        ModelAndView mav = new ModelAndView("register/register");
-        UserForm form = new UserForm();
-        form.setUsername(ex.getUsername());
-        form.setPassword(ex.getPassword());
-        form.setConfirmPassword(ex.getPassword());
-        form.setFirstName(ex.getFirstName());
-        form.setLastName(ex.getLastName());
-        mav.addObject("registerForm", form);
-        mav.addObject("duplicatedUsername", true);
-        return mav;
     }
 
     @ExceptionHandler(DuplicatedMailException.class)
