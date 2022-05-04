9bda1d1d273bfe3f77883b5d81773a07aa272667
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-05-04 14:55:08 -0300

    Add triggers for item ordering

--- persistence/src/main/resources/sql/schema6.sql
+++ persistence/src/main/resources/sql/schema6.sql
@@ -1,6 +1,6 @@
 ALTER TABLE menu_section DROP CONSTRAINT IF EXISTS menu_section_ordering_restaurant_id_key;
-ALTER TABLE menu_section DROP CONSTRAINT IF EXISTS menu_item_ordering_unique;
-ALTER TABLE menu_section ADD CONSTRAINT menu_item_ordering_unique UNIQUE(restaurant_id, ordering) DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE menu_section DROP CONSTRAINT IF EXISTS menu_section_ordering_unique;
+ALTER TABLE menu_section ADD CONSTRAINT menu_section_ordering_unique UNIQUE(restaurant_id, ordering) DEFERRABLE INITIALLY DEFERRED;
 
 -- Automatically order into last place for new sections
 CREATE OR REPLACE FUNCTION insert_section_ordering()
@@ -65,3 +65,72 @@ CREATE TRIGGER update_item_section_trigger
     FOR EACH ROW
     WHEN (pg_trigger_depth() < 1)
 EXECUTE PROCEDURE update_section_ordering();
+
+-- ITEM
+ALTER TABLE menu_item DROP CONSTRAINT IF EXISTS menu_item_ordering_section_id_key;
+ALTER TABLE menu_item DROP CONSTRAINT IF EXISTS menu_item_ordering_unique;
+ALTER TABLE menu_item ADD CONSTRAINT menu_item_ordering_unique UNIQUE(section_id, ordering) DEFERRABLE INITIALLY DEFERRED;
+
+-- Automatically order into last place for new sections
+CREATE OR REPLACE FUNCTION insert_item_ordering()
+    RETURNS TRIGGER AS $$
+BEGIN
+    SELECT COUNT(*) + 1 INTO NEW.ordering FROM menu_item WHERE section_id = NEW.section_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS insert_item_trigger ON menu_item;
+CREATE TRIGGER insert_item_trigger
+    BEFORE INSERT on menu_item
+    FOR EACH ROW
+EXECUTE PROCEDURE insert_item_ordering();
+
+-- Automatically rearrangement of orders when deletion
+CREATE OR REPLACE FUNCTION delete_item_ordering()
+    RETURNS TRIGGER AS $$
+DECLARE
+    cursor CURSOR FOR SELECT *
+                      FROM menu_item
+                      WHERE section_id = OLD.section_id AND ordering >= OLD.ordering
+                      ORDER BY ordering
+                          FOR UPDATE;
+    rec RECORD;
+BEGIN
+    OPEN cursor;
+    FETCH cursor INTO rec;
+    WHILE FOUND LOOP
+            UPDATE menu_item SET ordering = ordering - 1 WHERE CURRENT OF cursor;
+            FETCH cursor INTO rec;
+        END LOOP;
+    CLOSE cursor;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS delete_item_trigger ON menu_item;
+CREATE TRIGGER delete_item_trigger
+    AFTER DELETE on menu_item
+    FOR EACH ROW
+EXECUTE PROCEDURE delete_item_ordering();
+
+-- Automatically swap orders
+CREATE OR REPLACE FUNCTION update_item_ordering()
+    RETURNS TRIGGER AS $$
+DECLARE
+    cursor CURSOR FOR SELECT *
+                      FROM menu_item
+                      WHERE section_id = OLD.section_id AND ordering = NEW.ordering;
+    rec RECORD;
+BEGIN
+    UPDATE menu_item SET ordering = OLD.ordering WHERE section_id = OLD.section_id AND ordering = NEW.ordering;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_item_trigger ON menu_item;
+CREATE TRIGGER update_item_trigger
+    BEFORE UPDATE on menu_item
+    FOR EACH ROW
+    WHEN (pg_trigger_depth() < 1)
+EXECUTE PROCEDURE update_item_ordering();
