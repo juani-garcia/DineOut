f911697a44d78aa458a2473e89a86a4e25507881
BraveJero <jbrave271@gmail.com>
2022-05-04 18:03:31 -0300

    Uodate schema and add form

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/FavoriteForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/FavoriteForm.java
@@ -2,4 +2,14 @@ package ar.edu.itba.paw.webapp.form;
 
 public class FavoriteForm {
 
+    private boolean upVote = false;
+
+    public boolean isUpVote() {
+        return upVote;
+    }
+
+    public void setUpVote(boolean upVote) {
+        this.upVote = upVote;
+    }
+
 }
