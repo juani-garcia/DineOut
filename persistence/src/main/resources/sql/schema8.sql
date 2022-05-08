712328b56b9a1cf686b2589720b68e4b7a338285
BraveJero <jbrave271@gmail.com>
2022-05-08 10:30:11 -0300

    Update password_reset_token table to use isUsed

--- persistence/src/main/resources/sql/schema8.sql
+++ persistence/src/main/resources/sql/schema8.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS password_reset_token (
     token TEXT,
     user_id INTEGER NOT NULL,
     expiry_date TIMESTAMP NOT NULL,
+    is_used BOOLEAN DEFAULT false,
     FOREIGN KEY (user_id) REFERENCES account(id),
     PRIMARY KEY (id)
 );
