b15d6138679a0f59f44d7ce873e8113dd850139b
BraveJero <jbrave271@gmail.com>
2022-05-07 21:47:01 -0300

    Add basic forgot and update pass forms

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/NewPasswordForm.java
@@ -0,0 +1,34 @@
+package ar.edu.itba.paw.webapp.form;
+
+public class NewPasswordForm {
+    // Add validators
+    private String oldPassword;
+
+    private  String token;
+
+    private String newPassword;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
