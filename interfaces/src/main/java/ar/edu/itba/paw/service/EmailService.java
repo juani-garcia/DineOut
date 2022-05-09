73bdec39a4d5052f9e5260457f02db6476b62014
shoni <juanigarcia@itba.edu.ar>
2022-05-08 21:27:26 -0300

    Add methods to send specific mails

--- interfaces/src/main/java/ar/edu/itba/paw/service/EmailService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/service/EmailService.java
@@ -1,9 +1,17 @@
 package ar.edu.itba.paw.service;
 
+import ar.edu.itba.paw.persistence.Reservation;
+import ar.edu.itba.paw.persistence.User;
+
 import java.time.LocalDateTime;
 
 public interface EmailService {
 
     void sendReservationToRestaurant(long reservation_id, String to, String from, int amount, LocalDateTime when,  String comments);
 
+    void sendAccountCreationMail(String to, String name);
+
+    void sendReservationCancelledUser(String to, String name, Reservation reservation);
+
+    void sendReservationCancelledRestaurant(String to, String name, Reservation reservation, User user);
 }
--- service/src/main/java/ar/edu/itba/paw/service/EmailServiceImpl.java
+++ service/src/main/java/ar/edu/itba/paw/service/EmailServiceImpl.java
@@ -1,11 +1,20 @@
 package ar.edu.itba.paw.service;
 
+import ar.edu.itba.paw.persistence.Reservation;
+import ar.edu.itba.paw.persistence.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring4.SpringTemplateEngine;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class EmailServiceImpl implements EmailService {
@@ -13,6 +22,8 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Autowired
+    private SpringTemplateEngine thymeleafTemplateEngine;
 
     @Override
     public void sendReservationToRestaurant(long reservation_id, String to, String user, int amount, LocalDateTime when, String comments) {
@@ -29,6 +40,60 @@ public class EmailServiceImpl implements EmailService {
                 "Comentarios: " + amount + '\n';
 
         message.setText(body);
+        emailSender.send(message);
+    }
+
+    @Override
+    public void sendAccountCreationMail(String to, String name) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("recipient", name);
+        sendMessageUsingThymeleafTemplate(to, model, "account-creation.html");
+    }
+
+    @Override
+    public void sendReservationCancelledUser(String to, String name, Reservation reservation) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("recipient", name);
+        model.put("date", reservation.getDateString());
+        model.put("time", reservation.getTimeString());
+        model.put("restaurant", reservation.getRestaurant().getName());
+        sendMessageUsingThymeleafTemplate(to, model, "reservation-cancelled-user.html");
+    }
+
+    @Override
+    public void sendReservationCancelledRestaurant(String to, String name, Reservation reservation, User user) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("recipient", reservation.getRestaurant().getName());
+        model.put("date", reservation.getDateString());
+        model.put("time", reservation.getTimeString());
+        model.put("firstName", user.getFirstName());
+        model.put("lastName", user.getFirstName());
+        sendMessageUsingThymeleafTemplate(to, model, "reservation-cancelled-restaurant.html");
+    }
+
+    private void sendMessageUsingThymeleafTemplate(String to, Map<String, Object> templateModel, String template) {
+
+        Context thymeleafContext = new Context();
+        thymeleafContext.setVariables(templateModel);
+        String htmlBody = thymeleafTemplateEngine.process(template, thymeleafContext);
+
+        try {
+            sendHtmlMessage(to, htmlBody);
+        } catch(MessagingException mex) {
+            // TODO: Add to logger that mail could not be sent
+            System.out.println("*********************************");
+            System.out.println("MAIL COULD NOT BE SENT");
+            System.out.println("*********************************");
+        }
+    }
+
+    private void sendHtmlMessage(String to, String htmlBody) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo(to);
+        helper.setFrom("dineout.graphene@gmail.com");
+        helper.setSubject("DineOut");
+        helper.setText(htmlBody, true);
         emailSender.send(message);
     }
 }
