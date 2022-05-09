46f6e3c059582170b0e8fbd4490781c29951d02f
BraveJero <jbrave271@gmail.com>
2022-05-09 19:55:12 -0300

    Add password validation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/NewPasswordForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/NewPasswordForm.java
@@ -1,16 +1,16 @@
 package ar.edu.itba.paw.webapp.form;
 
 import ar.edu.itba.paw.webapp.validations.FieldsValueMatch;
+import ar.edu.itba.paw.webapp.validations.Password;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @FieldsValueMatch(
         field = "password",
         fieldMatch = "confirmPassword"
 )
 public class NewPasswordForm {
-    @Size(min = 8)
+    @Password
     private String password;
 
     @NotNull
