05d7505e8e3df380a04279f2bec6ee43dd825179
BraveJero <jbrave271@gmail.com>
2022-05-11 00:06:51 -0300

    Update schema

--- persistence/src/main/resources/sql/schema7.sql
+++ persistence/src/main/resources/sql/schema7.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS favorite (
     restaurant_id INTEGER,
     user_id INTEGER,
     FOREIGN KEY (restaurant_id) REFERENCES restaurant(id),
-    FOREIGN KEY (user_id) REFERENCES restaurant(id),
+    FOREIGN KEY (user_id) REFERENCES account(id),
     PRIMARY KEY (restaurant_id, user_id)
 );
