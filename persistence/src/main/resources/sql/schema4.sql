8a88177b8ef27477b984a8b8c1762b7d4de1d637
shoni <juanigarcia@itba.edu.ar>
2022-05-11 16:29:26 -0300

    Update schemas

--- persistence/src/main/resources/sql/schema4.sql
+++ persistence/src/main/resources/sql/schema4.sql
@@ -1,7 +1,8 @@
 -- Add first and last name to account.
-ALTER TABLE account ADD COLUMN first_name TEXT NOT NULL DEFAULT '';
-ALTER TABLE account ADD COLUMN last_name TEXT NOT NULL DEFAULT '';
+ALTER TABLE account ADD COLUMN IF NOT EXISTS first_name TEXT NOT NULL DEFAULT '';
+ALTER TABLE account ADD COLUMN IF NOT EXISTS last_name TEXT NOT NULL DEFAULT '';
 
 -- Add image to menu_item
-ALTER TABLE menu_item ADD COLUMN image_id INTEGER DEFAULT null;
+ALTER TABLE menu_item ADD COLUMN IF NOT EXISTS image_id INTEGER DEFAULT null;
+ALTER TABLE menu_item DROP CONSTRAINT IF EXISTS menu_item_image_id_fkey;
 ALTER TABLE menu_item ADD FOREIGN KEY (image_id) REFERENCES image(id) ON DELETE SET null;
