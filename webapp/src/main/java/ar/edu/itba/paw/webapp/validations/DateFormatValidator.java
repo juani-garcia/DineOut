68dc733280ee36b14507ba2c62d6924368f7b2e6
shoni <juanigarcia@itba.edu.ar>
2022-05-11 10:07:18 -0300

    Parametrize format validator

--- webapp/src/main/java/ar/edu/itba/paw/webapp/validations/DateFormatValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validations/DateFormatValidator.java
@@ -3,13 +3,17 @@ package ar.edu.itba.paw.webapp.validations;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
-public class FormatValidator implements ConstraintValidator<Format, String> {
+public class DateFormatValidator implements ConstraintValidator<DateFormat, String> {
+
+    private DateTimeFormatter formatter;
 
     @Override
-    public void initialize(Format format) {
-
+    public void initialize(DateFormat constraintAnnotation) {
+        formatter = DateTimeFormatter.ofPattern(constraintAnnotation.format());
     }
 
     @Override
@@ -17,7 +21,7 @@ public class FormatValidator implements ConstraintValidator<Format, String> {
         if(s == null) return false;
 
         try {
-            LocalDate.parse(s);
+            LocalDate.parse(s, formatter);
         } catch (DateTimeParseException e) {
             return false;
         }
