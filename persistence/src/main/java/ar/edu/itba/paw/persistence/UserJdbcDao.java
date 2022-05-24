ca5b1b55ffc55913b68c8d52e684b4e48f2d4d55
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-05-24 12:26:14 -0300

    Implement UserJPADao

--- persistence/src/main/java/ar/edu/itba/paw/persistence/UserJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserJdbcDao.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Repository
+// @Repository
 public class UserJdbcDao implements UserDao {
 
     private final JdbcTemplate jdbcTemplate;
