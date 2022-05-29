d3fb26f409c8f6e4a4dd4f698ec6d63c248aa446
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-05-29 18:13:49 -0300

    Migrate user role management to hibernate

--- model/src/main/java/ar/edu/itba/paw/model/User.java
+++ model/src/main/java/ar/edu/itba/paw/model/User.java
@@ -24,6 +24,14 @@ public class User {
     @Column(nullable = false, name = "last_name")
     private String lastName;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "account_to_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Collection<UserRole> roles;
+
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinTable(
             name = "favorite",
@@ -32,7 +40,6 @@ public class User {
     private Collection<Restaurant> favorites;
 
     protected User() {
-
     }
 
     public User(final String username, final String password, final String firstName, final String lastName) {
@@ -71,8 +78,16 @@ public class User {
         return lastName;
     }
 
+    public Collection<UserRole> getRoles() {
+        return roles;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void addRole(UserRole userRole) {
+        this.roles.add(userRole);
     }
 
     public void addFavorite(Restaurant restaurant) {
