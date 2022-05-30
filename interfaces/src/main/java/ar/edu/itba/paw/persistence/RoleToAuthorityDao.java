9a676ab224d0e96a8b73d03f98fae3ae9d46a8d7
BraveJero <jbrave271@gmail.com>
2022-05-30 17:25:00 -0300

    remove unused files

--- interfaces/src/main/java/ar/edu/itba/paw/persistence/RoleToAuthorityDao.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.itba.paw.persistence;
-
-import ar.edu.itba.paw.model.RoleToAuthority;
-
-import java.util.List;
-
-public interface RoleToAuthorityDao {
-    List<RoleToAuthority> getByRoleId(final long roleId);
-}
