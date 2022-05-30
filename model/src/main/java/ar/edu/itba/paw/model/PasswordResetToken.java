0c9c86ae71d56273e7b23fabd7ad1d3cee19f347
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-05-30 16:43:18 -0300

    Improve database restrictions for token and add use method

--- model/src/main/java/ar/edu/itba/paw/model/PasswordResetToken.java
+++ model/src/main/java/ar/edu/itba/paw/model/PasswordResetToken.java
@@ -19,7 +19,7 @@ public class PasswordResetToken {
 
     // TODO: Check orphanRemoval
     @ManyToOne(optional = false)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(name = "expiry_date", nullable = false)
@@ -65,6 +65,10 @@ public class PasswordResetToken {
 
     public boolean isUsed() {
         return isUsed;
+    }
+
+    public void use() {
+        isUsed = true;
     }
 
 }
