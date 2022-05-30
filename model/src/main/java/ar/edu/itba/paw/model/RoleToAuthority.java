78b637eb815f019ef843ca1d6bcfd84e1a50fc46
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-05-29 23:10:39 -0300

    Create RoleToAuthority entity from model

--- model/src/main/java/ar/edu/itba/paw/model/RoleToAuthority.java
+++ model/src/main/java/ar/edu/itba/paw/model/RoleToAuthority.java
@@ -1,23 +1,42 @@
 package ar.edu.itba.paw.model;
 
-public class RoleToAuthority {
-    public final long id, authorityId, roleId;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "role_to_authority")
+public class RoleToAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_to_authority_id_seq")
+    @SequenceGenerator(allocationSize = 1, sequenceName = "role_to_authority_id_seq", name = "role_to_authority_id_seq")
+    private Long id;
+
+    @Column(name = "authority_id", nullable = false)
+    private Long authorityId;
+
+    @Column(name = "role_id", nullable = false)
+    private Long roleId;
+
+    RoleToAuthority() {
+    }
+
+    @Deprecated
     public RoleToAuthority(long id, long authorityId, long roleId) {
         this.id = id;
         this.authorityId = authorityId;
         this.roleId = roleId;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public long getAuthorityId() {
+    public Long getAuthorityId() {
         return authorityId;
     }
 
-    public long getRoleId() {
+    public Long getRoleId() {
         return roleId;
     }
+
 }
