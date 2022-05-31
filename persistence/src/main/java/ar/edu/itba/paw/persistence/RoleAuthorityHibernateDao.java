86ea598a38309b6985d053458f0688527ba1763c
BraveJero <jbrave271@gmail.com>
2022-05-31 11:32:31 -0300

    Add authorities getter

--- persistence/src/main/java/ar/edu/itba/paw/persistence/RoleAuthorityHibernateDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/RoleAuthorityHibernateDao.java
@@ -1,10 +1,13 @@
 package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.model.RoleAuthority;
+import ar.edu.itba.paw.model.UserRole;
+import ar.edu.itba.paw.model.exceptions.NotFoundException;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
@@ -16,6 +19,14 @@ public class RoleAuthorityHibernateDao implements RoleAuthorityDao {
     @Override
     public Optional<RoleAuthority> getByAuthorityId(final long authorityId) {
         return Optional.ofNullable(em.find(RoleAuthority.class, authorityId));
+    }
+
+    @Override
+    public Collection<RoleAuthority> getAuthoritiesOf(Long roleId) {
+        Optional<UserRole> userRole = em.createQuery("SELECT r from UserRole r JOIN FETCH r.authorities WHERE r.id = :roleId", UserRole.class)
+                .setParameter("roleId", roleId)
+                .getResultList().stream().findFirst();
+        return userRole.orElseThrow(NotFoundException::new).getAuthorities();
     }
 
 }
