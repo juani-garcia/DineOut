e0cc88e309a009475ecd90c05d55254b843381c8
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-05-31 16:52:19 -0300

    Update dao to use new PagedQuery class

--- interfaces/src/main/java/ar/edu/itba/paw/persistence/ReservationDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/persistence/ReservationDao.java
@@ -1,26 +1,23 @@
 package ar.edu.itba.paw.persistence;
 
+import ar.edu.itba.paw.model.PagedQuery;
 import ar.edu.itba.paw.model.Reservation;
 import ar.edu.itba.paw.model.Restaurant;
 import ar.edu.itba.paw.model.User;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface ReservationDao {
 
     Reservation create(Restaurant restaurant, User user, int amount, LocalDateTime dateTime, String comments);
 
-    List<Reservation> getAllByUsername(String username, int page, boolean past);
+    Optional<Reservation> getReservation(long reservationId);
 
-    List<Reservation> getAllByRestaurant(long restaurantId, int page, boolean past);
+    PagedQuery<Reservation> getAllByUsername(String username, int page, boolean past);
+
+    PagedQuery<Reservation> getAllByRestaurant(long restaurantId, int page, boolean past);
 
     void delete(long reservationId);
 
-    Optional<Reservation> getReservation(long reservationId);
-
-    long getPagesCountForCurrentUser(String username, boolean past);
-
-    long getPagesCountForCurrentRestaurant(Restaurant self, boolean past);
 }
