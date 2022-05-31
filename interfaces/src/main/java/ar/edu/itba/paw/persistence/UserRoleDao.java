d4449199dc789a9adc0daecd9330a6574c32aa9b
BraveJero <jbrave271@gmail.com>
2022-05-31 11:33:19 -0300

    add getter to interface

--- interfaces/src/main/java/ar/edu/itba/paw/persistence/UserRoleDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/persistence/UserRoleDao.java
@@ -2,10 +2,13 @@ package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.model.UserRole;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface UserRoleDao {
     Optional<UserRole> getByRoleId(final long roleId);
 
     Optional<UserRole> getByRoleName(String roleName);
+
+    Collection<UserRole> getRolesOf(Long userId);
 }
