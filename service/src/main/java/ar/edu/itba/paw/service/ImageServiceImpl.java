3db3233a1d61a5171476ceca2d1978ec328024ee
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2022-05-31 23:54:03 -0300

    Make imageService methods transactional

--- service/src/main/java/ar/edu/itba/paw/service/ImageServiceImpl.java
+++ service/src/main/java/ar/edu/itba/paw/service/ImageServiceImpl.java
@@ -5,6 +5,8 @@ import ar.edu.itba.paw.model.exceptions.NotFoundException;
 import ar.edu.itba.paw.persistence.ImageDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 @Service
@@ -17,6 +19,7 @@ public class ImageServiceImpl implements ImageService {
         this.imageDao = imageDao;
     }
 
+    @Transactional
     @Override
     public Image create(final byte[] source) {
         return imageDao.create(source);
@@ -27,12 +30,14 @@ public class ImageServiceImpl implements ImageService {
         return imageDao.getById(id);
     }
 
+    @Transactional
     @Override
     public void edit(final long id, final byte[] source) {
         Image image = getById(id).orElseThrow(NotFoundException::new);
         image.setSource(source);
     }
 
+    @Transactional
     @Override
     public void delete(final long id) {
         imageDao.delete(id);
