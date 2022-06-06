d321a7d392dbf50e03f524ebed87acba7739923d
BraveJero <jbrave271@gmail.com>
2022-06-05 22:19:30 -0300

    Update dbs

--- persistence/src/test/resources/sql/hsqldb4.sql
+++
@@ -1,5 +0,0 @@
--- Add first and last name to account.
-
--- Add image to menu_item
-ALTER TABLE menu_item ADD COLUMN image_id INTEGER DEFAULT null;
-ALTER TABLE menu_item ADD FOREIGN KEY (image_id) REFERENCES image(id) ON DELETE CASCADE;
