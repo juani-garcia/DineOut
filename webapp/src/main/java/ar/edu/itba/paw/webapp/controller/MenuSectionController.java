664264771e79b731706e9d7c1694cd04b0cde596
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-04 18:13:01 -0300

    Implement menu section creation and update

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MenuSectionController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MenuSectionController.java
@@ -4,14 +4,20 @@ import ar.edu.itba.paw.model.MenuSection;
 import ar.edu.itba.paw.service.MenuSectionService;
 import ar.edu.itba.paw.webapp.dto.MenuSectionDTO;
 import ar.edu.itba.paw.webapp.dto.RestaurantDTO;
+import ar.edu.itba.paw.webapp.form.MenuSectionForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Component;
 
+import javax.print.attribute.standard.Media;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import java.awt.*;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -47,6 +53,16 @@ public class MenuSectionController {
         return Response.ok(new GenericEntity<List<MenuSectionDTO>>(menuSectionDTOList){}).build();
     }
 
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response createMenuSection(
+            @Valid final MenuSectionForm menuSectionForm
+    ) {
+        final MenuSection menuSection = mss.create(menuSectionForm.getName());
+        final URI location = uriInfo.getAbsolutePathBuilder().path(String.valueOf(menuSection.getId())).build();
+        return Response.created(location).build();
+    }
+
     @GET
     @Path("/{id}")
     @Produces({MediaType.APPLICATION_JSON})
@@ -58,6 +74,17 @@ public class MenuSectionController {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.ok(maybeMenuSection.get()).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response updateMenuSection(
+            @PathParam("id") final long menuSectionId,
+            @Valid final MenuSectionForm menuSectionForm
+    ) {
+        mss.updateName(menuSectionId, menuSectionForm.getName());
+        return Response.ok().build();
     }
 
     @DELETE
