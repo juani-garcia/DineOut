b2fb538a872853c6484051641a92b4d5e26e1265
shoni <juanigarcia@itba.edu.ar>
2023-07-05 23:35:55 -0300

    Add endpoints [wip]

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
@@ -8,32 +8,40 @@ import ar.edu.itba.paw.service.UserService;
 import ar.edu.itba.paw.webapp.dto.UserDTO;
 import ar.edu.itba.paw.webapp.form.UserForm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Path("users")
 @Component
 public class UserController {
-
-    private SecurityService securityService;
     private UserService userService;
+    private SecurityService securityService;
 
     @Context
     private UriInfo uriInfo;
 
     @Autowired
-    public UserController(SecurityService securityService,
-                          UserService userService) {
-        this.securityService = securityService;
+    public UserController(UserService userService,
+                          SecurityService securityService) {
         this.userService = userService;
+        this.securityService = securityService;
+    }
+
+    @GET
+    @Path("/{id}")
+    @PreAuthorize("@securityManager.validateAuthById(authentication, #userID)")
+    public Response readUser(@PathParam("id") final long userID) {
+        Optional<UserDTO> maybeUser = userService.getById(userID).map(u -> UserDTO.fromUser(uriInfo, u));
+        if (! maybeUser.isPresent()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(maybeUser.get()).build();
     }
 
     @POST
@@ -50,14 +58,25 @@ public class UserController {
         return Response.created(location).build();
     }
 
-    @GET
+    @PUT
     @Path("/{id}")
-    public Response readUser(@PathParam("id") final long userID) {
-        Optional<UserDTO> maybeUser = userService.getById(userID).map(u -> UserDTO.fromUser(uriInfo, u));
-        if (! maybeUser.isPresent()) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-        return Response.ok(maybeUser.get()).build();
+    @Consumes(value = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED, })
+    public Response editUser(@Valid final UserForm userForm) {
+        final Optional<User> current = securityService.getCurrentUser();
+
+        if(!current.isPresent())
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+
+        final Optional<User> edited = userService.edit(
+                current.get(),
+                userForm.getFirstName(),
+                userForm.getLastName(),
+                uriInfo.getPath());
+
+        if(!edited.isPresent())
+            return Response.status(Response.Status.FORBIDDEN).build();
+
+        return Response.noContent().build();
     }
 
 }
