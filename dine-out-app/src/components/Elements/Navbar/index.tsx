1a8edd57d287614962606f7e4f3c2032268fceba
BraveJero <jbrave271@gmail.com>
2023-07-06 19:54:28 -0300

    wip login form

--- dine-out-app/src/components/Elements/Navbar/index.tsx
+++ dine-out-app/src/components/Elements/Navbar/index.tsx
@@ -11,9 +11,11 @@ import {
 } from './styles'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../../hooks/auth/useAuth'
 
 function Navbar (): JSX.Element {
   const { t } = useTranslation()
+  const { user } = useAuth()
 
   return (
         <Navigation>
@@ -21,17 +23,22 @@ function Navbar (): JSX.Element {
                 <NavTitle id="logo-container" href="#" as={Link} to="/">Dine Out
                     <Logo src="https://images.emojiterra.com/google/android-pie/512px/1f35c.png"/>
                 </NavTitle>
-                <NavRight>
-                    {/*    TODO: if logged in show "Hello {user}!" */}
-                    <ListItem>
-                        <RegisterButton>{t('register')}</RegisterButton>
-                    </ListItem>
-                    <ListItem>
-                        <LoginButton as={Link} to="/login">
-                            {t('login')}
-                        </LoginButton>
-                    </ListItem>
-                </NavRight>
+
+                {user == null
+                  ? <NavRight>
+                        <ListItem>
+                            <RegisterButton>{t('register')}</RegisterButton>
+                        </ListItem>
+                        <ListItem>
+                            <LoginButton as={Link} to="/login">
+                                {t('login')}
+                            </LoginButton>
+                        </ListItem>
+                    </NavRight>
+                  : <NavRight>
+                        <ListItem>Boeeenas ${user.sub}</ListItem>
+                    </NavRight>}
+
             </NavigationContainer>
         </Navigation>
   )
--- dine-out-app/src/components/Pages/Login/index.tsx
+++ dine-out-app/src/components/Pages/Login/index.tsx
@@ -1,14 +1,58 @@
 import React from 'react'
 import { MyContainer, Title, WhiteBoxContainer } from '../../Elements/utils/styles'
 import { useTranslation } from 'react-i18next'
+import { LoginForm } from './styles'
+import { Button, Checkbox, FormControl, FormControlLabel, TextField } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import { useLogin } from '../../../hooks/auth/useLogin'
 
 function Login (): JSX.Element {
   const { t } = useTranslation()
 
+  const { handleSubmit, control } = useForm()
+  const { login } = useLogin()
+
+  const onLogin = (data: any): void => {
+    console.log('Submitted values:', data)
+    login(data.username, data.password).then((response) => {
+      console.log('BOCAAAAAAAAAAAAAAAAAAAa')
+      console.log(response)
+    }).catch((e) => {
+      console.log('ta to mal flaco')
+      console.log(e.data)
+    })
+  }
+
   return (
         <MyContainer>
             <Title>{t('login')}</Title>
-            <WhiteBoxContainer>BOCA</WhiteBoxContainer>
+            <WhiteBoxContainer>
+                <LoginForm>
+                    {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+                    <FormControl component="form" onSubmit={handleSubmit(onLogin)}>
+                        <TextField
+                            label="Username"
+                            fullWidth
+                            margin="normal"
+                            {...control.register('username')}
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            fullWidth
+                            margin="normal"
+                            {...control.register('password')}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox {...control.register('rememberMe')} />}
+                            label="Remember me"
+                        />
+                        <Button type="submit" variant="contained" color="primary">
+                            Submit
+                        </Button>
+                    </FormControl>
+                </LoginForm>
+            </WhiteBoxContainer>
         </MyContainer>
   )
 }
