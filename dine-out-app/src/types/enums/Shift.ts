15bd4b223fa36cc76f43ca5c4562e7a02876b6d0
BraveJero <jbrave271@gmail.com>
2023-07-12 15:33:34 -0300

    jaja mateo comprate una compu tonto

--- dine-out-app/src/types/enums/Shift.ts
+++ dine-out-app/src/types/enums/Shift.ts
@@ -1,15 +1,14 @@
 export class Shift {
-    static readonly MORNING = new Shift('MORNING', 'shifts.morning', 8, 12);
-    static readonly NOON = new Shift('NOON', 'shifts.noon', 12, 16);
-    static readonly AFTERNOON = new Shift('AFTERNOON', 'shifts.afternoon', 16, 20);
-    static readonly EVENING = new Shift('EVENING', 'shifts.evening', 20, 24);
+  static readonly MORNING = new Shift('MORNING', 'shifts.morning', 8, 12)
+  static readonly NOON = new Shift('NOON', 'shifts.noon', 12, 16)
+  static readonly AFTERNOON = new Shift('AFTERNOON', 'shifts.afternoon', 16, 20)
+  static readonly EVENING = new Shift('EVENING', 'shifts.evening', 20, 24)
 
-    private constructor(private readonly name: string, private readonly description: string, public readonly startingHour: number, public readonly closingHour: number) {
+  private constructor (private readonly name: string, private readonly description: string, public readonly startingHour: number, public readonly closingHour: number) {
 
-    }
+  }
 
-    toString() {
-        return this.description;
-    }
-    
+  toString (): string {
+    return this.description
+  }
 }
