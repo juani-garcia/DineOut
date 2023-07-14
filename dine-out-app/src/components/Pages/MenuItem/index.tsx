32765ee5fa8acfd0aa099ab4d6587216667b0ec6
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-14 10:22:30 -0300

    Add menu item creation page

---
+++ dine-out-app/src/components/Pages/MenuItem/index.tsx
@@ -0,0 +1,97 @@
+import { MyContainer, Title } from '@/components/Elements/utils/styles'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { MenuItemForm, Header, MenuItemWhiteBoxContainer } from './styles'
+import { Button, Checkbox, FormControl, FormControlLabel, TextField, InputLabel, Select, MenuItem, ListItemText, SelectChangeEvent } from '@mui/material'
+import { Controller, useForm } from 'react-hook-form'
+
+interface MenuItemFormInput {
+    name: string,
+    detail: string,
+    price: number,
+    section: string,
+    image?: string
+};
+
+const sections = [
+  "a",
+  "b",
+  "c"
+];
+
+export default function MenuItemCreation (): JSX.Element {
+  const { t } = useTranslation()
+  const { handleSubmit, control } = useForm()
+  const onSubmit = (data: any) => {
+    console.log(data)
+  };
+
+  return (
+    <MyContainer>
+        <Title>{t('MenuItem.form.title')}</Title>
+        <MenuItemWhiteBoxContainer>
+            <MenuItemForm onSubmit={handleSubmit(onSubmit)}>
+                <Header>{t('MenuItem.form.header')}</Header>
+                {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+                <FormControl component="form"
+                             sx={{
+                               '& .MuiButton-root': {
+                                 width: '40%'
+                               }
+                             }}>
+                    <TextField
+                        label={t('MenuItem.form.name')}
+                        fullWidth
+                        margin="normal"
+                        {...control.register('name')}
+                        variant="standard"
+                    />
+                    <TextField
+                        label={t('MenuItem.form.detail')}
+                        fullWidth
+                        margin="normal"
+                        {...control.register('detail')}
+                        variant="standard"
+                    />
+                    <TextField
+                        label={t('MenuItem.form.price')}
+                        fullWidth
+                        margin="normal"
+                        type="number"
+                        {...control.register('price')}
+                        variant="standard"
+                    />
+                    <FormControl fullWidth>
+                        <Controller
+                            name="section"
+                            control={control}
+                            rules={{ required: true }}
+                            defaultValue=""
+                            render={({ field }) => (
+                            <Select {...field} id="section-select" fullWidth label={t('MenuItem.form.section')}>
+                                {sections.map((option) => (
+                                <MenuItem key={option} value={option}>
+                                    {option}
+                                </MenuItem>
+                                ))}
+                            </Select>
+                            )}
+                        />
+                    </FormControl>
+                    <TextField
+                        label={t('MenuItem.form.image')}
+                        fullWidth
+                        margin="normal"
+                        type="file"
+                        {...control.register('image')}
+                        variant="standard"
+                    />
+                    <Button type="submit" variant="contained" color="primary">
+                        {t('MenuItem.form.submit')}
+                    </Button>
+                </FormControl>
+            </MenuItemForm>
+        </MenuItemWhiteBoxContainer>
+    </MyContainer>
+  )
+}
