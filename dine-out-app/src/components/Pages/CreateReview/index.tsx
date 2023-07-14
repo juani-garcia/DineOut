6cb3271d5144eb76bae3002b8b7898177a3ae889
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-14 10:31:22 -0300

    Create review page

---
+++ dine-out-app/src/components/Pages/CreateReview/index.tsx
@@ -0,0 +1,63 @@
+import { MyContainer, Title } from '@/components/Elements/utils/styles'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { ReviewWhiteBoxContainer, ReviewForm, Header } from './styles'
+import { Button, Checkbox, FormControl, FormControlLabel, TextField, InputLabel, Select, MenuItem, ListItemText, SelectChangeEvent } from '@mui/material'
+import { useForm, Controller } from 'react-hook-form'
+import internal from 'stream'
+
+interface ReviewCreationForm {
+    review: string,
+    rating: number
+};
+
+export default function Review (): JSX.Element {
+  const { t } = useTranslation()
+  const { handleSubmit, control } = useForm()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  };
+
+  return (
+    <MyContainer>
+        <Title>{t('Review.create.title')}</Title>
+        <ReviewWhiteBoxContainer>
+            <ReviewForm onSubmit={handleSubmit(onSubmit)}>
+                <Header>{t('Review.create.header')}</Header>
+                {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+                <FormControl component="form"
+                             sx={{
+                               '& .MuiButton-root': {
+                                 width: '40%'
+                               }
+                             }}>
+                    <TextField
+                        label={t('Review.create.review')}
+                        fullWidth
+                        margin="normal"
+                        {...control.register('review')}
+                        variant="standard"
+                    />
+                    <TextField
+                        label={t('Register-restaurant.rating')}
+                        fullWidth
+                        margin="normal"
+                        type="number"
+                        {...control.register('rating')}
+                        variant="standard"
+                    />
+                    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                        <Button variant="contained" color="error">
+                            {t('Review.cancel')}
+                        </Button>
+                        <Button type="submit" variant="contained" color="primary">
+                            {t('Review.submit')}
+                        </Button>
+                    </div>
+                </FormControl>
+            </ReviewForm>
+        </ReviewWhiteBoxContainer>
+    </MyContainer>
+  )
+}
