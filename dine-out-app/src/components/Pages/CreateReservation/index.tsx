1c808c90f9a386354dcce1cb7140c015db159b6f
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-14 10:06:42 -0300

    Develop reservation creation page

---
+++ dine-out-app/src/components/Pages/CreateReservation/index.tsx
@@ -0,0 +1,91 @@
+import { MyContainer, Title } from '@/components/Elements/utils/styles'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { ReservationWhiteBoxContainer, ReservationForm, Header } from './styles'
+import { Button, Checkbox, FormControl, FormControlLabel, TextField, InputLabel, Select, MenuItem, ListItemText, SelectChangeEvent } from '@mui/material'
+import { useForm, Controller } from 'react-hook-form'
+import internal from 'stream'
+
+interface ReservationCreationForm {
+    amount: number,
+    date: Date,
+    hour: Date,
+    comments: string
+};
+
+const options: string[] = [
+    "1",
+    "2",
+    "3"
+];
+
+export default function Reservation (): JSX.Element {
+  const { t } = useTranslation()
+  const { handleSubmit, control } = useForm()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  };
+
+  return (
+    <MyContainer>
+        <Title>{t('Reservation.create.title')}</Title>
+        <ReservationWhiteBoxContainer>
+            <ReservationForm onSubmit={handleSubmit(onSubmit)}>
+                <Header>{t('Reservation.create.header')}</Header>
+                {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+                <FormControl component="form"
+                             sx={{
+                               '& .MuiButton-root': {
+                                 width: '40%'
+                               }
+                             }}>
+                    <TextField
+                        label={t('Reservation.create.amount')}
+                        fullWidth
+                        margin="normal"
+                        {...control.register('amount')}
+                        type='number'
+                        variant="standard"
+                    />
+                    <TextField
+                        label={t('Reservation.create.date')}
+                        fullWidth
+                        margin="normal"
+                        type='date'
+                        {...control.register('date')}
+                        variant="standard"
+                    />
+                    <FormControl fullWidth>
+                        <Controller
+                            name="time"
+                            control={control}
+                            rules={{ required: true }}
+                            defaultValue=""
+                            render={({ field }) => (
+                            <Select {...field} id="time-select" fullWidth label={t('Reservation.create.time')}>
+                                {options.map((option) => (
+                                <MenuItem key={option} value={option}>
+                                    {option}
+                                </MenuItem>
+                                ))}
+                            </Select>
+                            )}
+                        />
+                    </FormControl>
+                    <TextField
+                        label={t('Register-restaurant.comments')}
+                        fullWidth
+                        margin="normal"
+                        {...control.register('comments')}
+                        variant="standard"
+                    />
+                    <Button type="submit" variant="contained" color="primary">
+                        {t('Reservation.submit')}
+                    </Button>
+                </FormControl>
+            </ReservationForm>
+        </ReservationWhiteBoxContainer>
+    </MyContainer>
+  )
+}
--- dine-out-app/src/types/enums/Shift.ts
+++ dine-out-app/src/types/enums/Shift.ts
@@ -1,6 +1,26 @@
 export class Shift {
   static values: Shift[] = [];
 
+  static getSlotsFromShift(shift: Shift): string[] {
+    let slots: string[] = [];
+    const range = (start: number, end: number) =>
+      Array.from(Array(end - start + 1).keys()).map((x) => x + start);
+    range(shift.startingHour, shift.closingHour).forEach((hour) => {
+      slots.push(`${hour}:00`);
+      slots.push(`${hour}:30`);
+    });
+
+    return slots;
+  }
+
+  static getSlotsfromShiftArray(shifts: Shift[]): string[] {
+    let slots: string[] = [];
+    shifts
+      .map((shift) => this.getSlotsFromShift(shift))
+      .forEach((newSlots) => slots.concat(newSlots));
+    return slots;
+  }
+
   static readonly MORNING = new Shift("MORNING", "Shift.morning", 8, 12);
   static readonly NOON = new Shift("NOON", "Shift.noon", 12, 16);
   static readonly AFTERNOON = new Shift("AFTERNOON", "Shift.afternoon", 16, 20);
