403aba8eeef6f5beade0a5cd681ec6840560142f
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-17 19:19:36 -0300

    Update restaurant model

--- dine-out-app/src/types/models/Restaurant.ts
+++ dine-out-app/src/types/models/Restaurant.ts
@@ -1,14 +1,19 @@
 export default interface Restaurant {
-  address: string
-  detail: string
-  favCount: number
-  mail: string
-  name: string
-  lat: string
-  lng: string
-  owner: string
-  rating: number
-  ratingCount: number
-  self: string
-  zone: string
+  id: number;
+  address: string;
+  detail: string;
+  favCount: number;
+  mail: string;
+  name: string;
+  lat: string;
+  lng: string;
+  owner: string;
+  rating: number;
+  ratingCount: number;
+  self: string;
+  zone: string;
+  image: string;
+  shifts: string[];
+  categories: string[];
+  menuSections: string;
 }
