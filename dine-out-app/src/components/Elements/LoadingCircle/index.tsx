80ddae5c7f97a91dc5b96cb4384c7fd8b48a5055
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-17 14:26:51 -0300

    Create loading circle component

---
+++ dine-out-app/src/components/Elements/LoadingCircle/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { CircularProgress } from '@mui/material'
+
+export default function LoadingCircle (): JSX.Element {
+  return <CircularProgress color="secondary" size="100px"/>
+}
