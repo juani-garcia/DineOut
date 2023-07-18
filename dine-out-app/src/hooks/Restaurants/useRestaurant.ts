679a5eddcca6673c3ed037cbc5395da76fb45c41
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-17 21:19:42 -0300

    Implement hook for one restaurant

---
+++ dine-out-app/src/hooks/Restaurants/useRestaurant.ts
@@ -0,0 +1,19 @@
+import { useMethod } from "../auth/useMethod";
+import { paths } from "@/common/const";
+import { type AxiosResponse } from "axios";
+import { HttpMethod } from "@/types/enums/HTTPMethod";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function useRestaurant() {
+  const { isLoading, requestMethod } = useMethod();
+
+  async function restaurant(id: number): Promise<AxiosResponse> {
+    return await requestMethod({
+      method: HttpMethod.GET,
+      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+      url: `${paths.API_URL}${paths.RESTAURANTS}/${id}`,
+    });
+  }
+
+  return { isLoading, restaurant };
+}
