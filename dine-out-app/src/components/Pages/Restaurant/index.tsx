ec7b4195900511794f5179ad3f6dbb220ca4e7e9
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-17 23:52:51 -0300

    Implement restaurant detail oage

--- dine-out-app/src/components/Pages/Restaurant/index.tsx
+++ dine-out-app/src/components/Pages/Restaurant/index.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Menu from '@/components/Elements/Menu'
+import Restaurant from '@/types/models/Restaurant'
+import RestaurantBigCard from '@/components/Elements/RestaurantBigCard'
+import LoadingCircle from '@/components/Elements/LoadingCircle'
+import useRestaurant from '@/hooks/Restaurants/useRestaurant'
+import ReviewBigCard from '@/components/Elements/ReviewBigCard'
 
-function Restaurant (): JSX.Element {
-  const params = useParams()
-  return (<>{params.id}</>) // TODO: Check errors, 404, 403, etc.
+interface RestaurantProps {
+  restaurant?: Restaurant
 }
 
-export default Restaurant
+export default function RestaurantDetailPage ({ restaurant: restaurantProps }: RestaurantProps): JSX.Element {
+  const { isLoading, restaurant: requestRestaurant } = useRestaurant()
+  const [ restaurant, setRestaurant ] = useState<Restaurant>()
+  const params = useParams()
+
+  useEffect(() => {
+    if (restaurant) {
+      return
+    }
+    requestRestaurant(Number(params.id)).then(response => {
+      if (response.status === 404) {
+        // TODO: go to error page
+        setRestaurant(undefined)
+        return
+      }
+      setRestaurant(response.data as Restaurant)
+    }).catch(e => {
+      console.error(e.response)
+    })
+  }, [restaurant, params])
+
+  if (isLoading || !restaurant) {
+    return <LoadingCircle/>
+  }
+  // isLoading = false, restaurant defined
+  return (
+    <div style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+      <div style={{width: '40%'}}>
+        <RestaurantBigCard restaurant={restaurant}/>
+        <ReviewBigCard reviewListURI={restaurant.reviews}/>
+      </div>
+      <div style={{width: '40%'}}>
+        <Menu menuSectionsURI={restaurant.menuSections} />
+      </div>
+    </div>
+  ) // TODO: Check errors, 404, 403, etc.
+}
---
+++ dine-out-app/src/components/Pages/Restaurant/styles.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { WhiteBoxContainer } from '@/components/Elements/utils/styles'
+
+export const Container = styled(WhiteBoxContainer)`
+  width: 60%
+`
