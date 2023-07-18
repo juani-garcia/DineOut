7162da93e793aa0b8e6e2de1d365acc548168a36
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-07-17 21:31:45 -0300

    Add reviews to restaurant model

--- dine-out-app/src/types/models/Restaurant.ts
+++ dine-out-app/src/types/models/Restaurant.ts
@@ -1,19 +1,20 @@
 export default interface Restaurant {
-  id: number;
-  address: string;
-  detail: string;
-  favCount: number;
-  mail: string;
-  name: string;
-  lat: string;
-  lng: string;
-  owner: string;
-  rating: number;
-  ratingCount: number;
-  self: string;
-  zone: string;
-  image: string;
-  shifts: string[];
-  categories: string[];
-  menuSections: string;
+  id: number
+  address: string
+  detail: string
+  favCount: number
+  mail: string
+  name: string
+  lat: string
+  lng: string
+  owner: string
+  rating: number
+  ratingCount: number
+  self: string
+  zone: string
+  image: string
+  shifts: string[]
+  categories: string[]
+  menuSections: string
+  reviews: string
 }
