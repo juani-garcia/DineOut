47b878fb7aa46cc5070ed319ba352af72f752739
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-12-09 21:04:24 -0300

    Optimize imports

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/RestaurantUpdateForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/RestaurantUpdateForm.java
@@ -1,18 +1,6 @@
 package ar.edu.itba.paw.webapp.form;
 
-import ar.edu.itba.paw.model.Category;
-import ar.edu.itba.paw.model.Shift;
-import ar.edu.itba.paw.model.Zone;
-import ar.edu.itba.paw.webapp.validations.DuplicatedMail;
-import ar.edu.itba.paw.webapp.validations.FileSize;
-import ar.edu.itba.paw.webapp.validations.FileType;
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.hibernate.validator.constraints.Range;
-import org.springframework.web.multipart.MultipartFile;
-
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 public class RestaurantUpdateForm extends RestaurantForm {
